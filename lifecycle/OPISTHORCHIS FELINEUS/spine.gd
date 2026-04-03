471ab5933df5b634d4125e9ab9359b35e4276c11
Mudkip <nedotkajunio@gmail.com>
2026-04-03 21:51:34 +0500

    сделал модельки яйца и молюсков

---
+++ lifecycle/OPISTHORCHIS FELINEUS/spine.gd
@@ -0,0 +1,18 @@
+extends Line2D
+
+@onready var border = $border
+@onready var fill = $fill
+@onready var emb = $emb
+var spine_pts = self.points
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(spine_pts)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	border.points = self.points
+	fill.points = self.points.slice(1,-1)
+	emb.points = self.points.slice(7,19)
